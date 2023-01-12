009019e5d9871879f6cc1fe9971ff30519768d56
jcdsaez <jcdsaez@gmail.com>
2023-01-12 14:42:36 -0300

    funca pero no muestra

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http'
+import { Task } from '../Tasks'
+import { TASKS } from '../mock-tasks'
+import { Observable, of } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-type' : 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  constructor (
+    private http:HttpClient
+  ){}
+
+  deleteTask(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task:Task):Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
+  }
+
+  addTask(task:Task):Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task, httpOptions)
+  }
 }
+
