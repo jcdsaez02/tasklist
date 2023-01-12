2fc228916c47609912d071150947423679a75eb0
jcdsaez <jcdsaez@gmail.com>
2023-01-12 02:44:11 -0300

    mock up de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import {Tasks} from "./Tasks"
+
+export const TASKS: Tasks[] = [
+    {
+        id:1,
+        text:"texto 1",
+        day:"domingo 15",
+        reminder: true
+    },
+    {
+        id:2,
+        text:"texto 2",
+        day:"jueves 12",
+        reminder: false
+    },
+    {
+        id:3,
+        text:"texto 3",
+        day:"sabado 14",
+        reminder: true
+    },
+
+]
